fcb1017c035783e3b1958c17eea0da200078cb14
Silena Dangond <silenadangond1@gmail.com>
2021-11-12 22:57:10 -0500

    almost finish

--- script/script.js
+++ script/script.js
@@ -23,8 +23,7 @@ const showDataCities = async () => {
         city.addEventListener("click", () => {
             let city_selected = city.innerHTML
             console.log(city_selected);
-            search_input.value = city_selected
-            //search_city_selected(search_input.value);
+            search_input.value = city_selected;
         })
     });
 }
@@ -43,7 +42,7 @@ const search_city_selected = async (term, unit) => {
     card_information.setAttribute("id", "card_information");
     let x_icon = document.createElement("h3");
     x_icon.setAttribute("class", "x_icon");
-    x_icon.textContent = "X"
+    x_icon.textContent = "X";
     let name_city = document.createElement("h2");
     name_city.setAttribute("class", "cities_found name_city");
     name_city.textContent = res_city + `, ${res_country}`;
@@ -52,20 +51,54 @@ const search_city_selected = async (term, unit) => {
     temperature_number.textContent = res_temperature + '°';
     let unit_div = document.createElement("div");
     unit_div.setAttribute("id", "unit_div");
+
     let celsius_unit = document.createElement("li");
     celsius_unit.setAttribute("class", "unit_on");
-    celsius_unit.textContent = "C"
+    celsius_unit.setAttribute("id", "celsius");
+    celsius_unit.textContent = "C";
+
     let fahrenheit_unit = document.createElement("li");
     fahrenheit_unit.setAttribute("class", "unit_off");
-    fahrenheit_unit.textContent = "F"
+    fahrenheit_unit.setAttribute("id", "fahrenheit");
+    fahrenheit_unit.textContent = "F";
+
     let kelvin_unit = document.createElement("li");
     kelvin_unit.setAttribute("class", "unit_off");
-    kelvin_unit.textContent = "K"
+    kelvin_unit.setAttribute("id", "kelvin");
+    kelvin_unit.textContent = "K";
+
+    celsius_unit.addEventListener('click', () => {
+        celsius_unit.classList.remove("unit_off");
+        celsius_unit.classList.add("unit_on");
+        fahrenheit_unit.classList.remove("unit_on");
+        fahrenheit_unit.classList.add("unit_off");
+        kelvin_unit.classList.remove("unit_on");
+        kelvin_unit.classList.add("unit_off");
+    })
+
+    fahrenheit_unit.addEventListener('click', () => {
+        celsius_unit.classList.remove("unit_on");
+        celsius_unit.classList.add("unit_off");
+        fahrenheit_unit.classList.remove("unit_off");
+        fahrenheit_unit.classList.add("unit_on");
+        kelvin_unit.classList.remove("unit_on");
+        kelvin_unit.classList.add("unit_off");
+    })
+
+    kelvin_unit.addEventListener('click', () => {
+        celsius_unit.classList.remove("unit_on");
+        celsius_unit.classList.add("unit_off");
+        fahrenheit_unit.classList.remove("unit_on");
+        fahrenheit_unit.classList.add("unit_off");
+        kelvin_unit.classList.remove("unit_off");
+        kelvin_unit.classList.add("unit_on");
+    })
 
     let img_weather = document.createElement("img");
     img_weather.setAttribute("class", "img_weather");
     let description_weather = document.createElement("h2");
     description_weather.setAttribute("class", "cities_found description_weather");
+    
     if (res_temperature <= 19) {
         img_weather.setAttribute("src", "./icons/icons/coldWeather.svg");
         description_weather.textContent = "Cold";
@@ -75,11 +108,24 @@ const search_city_selected = async (term, unit) => {
     } else if (res_temperature >= 26.1) {
         img_weather.setAttribute("src", "./icons/icons/hotWeather.svg");
         description_weather.textContent = "Hot";
-    }
+    };
 
     let btn_update = document.createElement("button");
     btn_update.setAttribute("class", "btn_update");
     btn_update.textContent = "Update";
+
+    btn_update.addEventListener('click', () => {
+        if (celsius_unit.classList.contains("unit_on")) {
+            console.log("celsius")
+            update_card(res_city, "metric", card_information);
+        } else if (fahrenheit_unit.classList.contains("unit_on")) {
+            console.log("fahrenheit")
+            update_card(res_city, "imperial", card_information);
+        } else if (kelvin_unit.classList.contains("unit_on")) {
+            console.log("kelvin")
+            update_card(res_city, "standard", card_information);
+        }
+    })
 
     section_cities.appendChild(card_information);
     card_information.appendChild(x_icon);
@@ -98,7 +144,18 @@ const search_city_selected = async (term, unit) => {
 magnifying.addEventListener('click', () => {
     search_city_selected(search_input.value, "metric");
     console.log(search_input.value);
-})
+});
+
+const update_card = async (term, unit, card) => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${term}&units=${unit}&appid=${API_KEY}`)
+    const responseJson = await response.json()
+    const temp_res_Json = Math.trunc(responseJson.main.temp)
+    $(card).find('#temperature_number')[0].innerText = temp_res_Json + '°';
+    console.log($(card).find('#temperature_number'))
+}
+
+
+
 
 
 
